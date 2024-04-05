75c166b03ce45695e9c40b4099d4636744e8761b
juescoryisus <jesuseduardog204@gmail.com>
2024-04-04 19:31:54 -0600

    primer repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Archivo_no_estructurado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(68, 51);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(106, 46);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(222, 51);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(106, 46);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(101, 122);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(166, 26);
+            this.txtDato.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 240);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtDato;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Archivo_no_estructurado
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "datos.txt"; // Ruta del archivo de datos
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string dataToSave = txtDato.Text;
+
+            txtDato.Clear();
+
+            // Escribir el texto en el archivo
+            File.WriteAllText(filePath, dataToSave);
+
+            MessageBox.Show("Datos guardados correctamente en el archivo.");
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                // Leer el texto desde el archivo
+                string loadedData = File.ReadAllText(filePath);
+
+                // Mostrar el texto en el cuadro de texto
+                txtDato.Text = loadedData;
+
+                MessageBox.Show("Datos cargados correctamente desde el archivo.");
+            }
+            else
+            {
+                MessageBox.Show("El archivo no existe.");
+            }
+        }
+    }
+}
